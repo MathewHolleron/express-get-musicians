6c7d4e7efecdb408218b0070694f8353290a348d
MathewHolleron <Mattyholleron@gmail.com>
2023-02-20 17:04:38 +0000

    created a get function for the musicians

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+app.get("/musicians", async (req, res) =>{
+    try {
+        const musicians = await Musician.findAll(); 
+        res.json(musicians); 
+      } catch (error) {
+        console.error(error);
+      }
+});
 
 app.listen(port, () => {
     sequelize.sync();
